fa026f49002d4c7a758f3b4f0e7eb8fcb6a90ca3
Erika Heidi <erika@erikaheidi.com>
2022-06-21 12:11:09 +0200

    Added interactive option to stencil build command

--- Command/Stencil/DefaultController.php
+++ Command/Stencil/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Minicli\Stencil;
 
 use Minicli\Command\CommandController;
+use Minicli\FileNotFoundException;
+use Minicli\Input;
 use Minicli\Stencil;
 
 class DefaultController extends CommandController
@@ -21,15 +23,56 @@ class DefaultController extends CommandController
         }
 
         $template = $this->getParam('template');
-        $params = $this->getParams();
-        $parsedContent = $stencil->applyTemplate($template, $params);
+
+        $parsedContent = $stencil->applyTemplate($template, $this->getTemplateValues($stencil, $template));
 
         if ($this->hasParam('output')) {
             file_put_contents($this->getParam('output'), $parsedContent);
+            if ($this->getApp()->config->debug) {
+                $this->getPrinter()->info("File saved at: " . $this->getParam('output'));
+            }
         }
 
         if ($this->getApp()->config->debug) {
             echo($parsedContent);
         }
+    }
+
+    /**
+     * @param Stencil $stencil
+     * @param string $templateName
+     * @return array
+     * @throws FileNotFoundException
+     */
+    public function getTemplateValues(Stencil $stencil, string $templateName): array
+    {
+        $params = $this->getParams();
+        if ($this->hasFlag('i') OR $this->hasFlag('interactive')) {
+            $this->getPrinter()->info('Stencil Wizard', true);
+            $this->getPrinter()->out("Building file based on: $templateName.tpl");
+            $this->getPrinter()->newline();
+
+            $variables = $stencil->scanTemplateVars($templateName);
+            $params = $this->inputCollect($variables);
+        }
+
+        return $params;
+    }
+
+    /**
+     * @param array $variables
+     * @return array
+     */
+    public function inputCollect(array $variables): array
+    {
+        $values = [];
+        $input = new Input(': ');
+
+        foreach ($variables as $variable) {
+            $this->getPrinter()->newline();
+            $this->getPrinter()->out($variable);
+            $values[$variable] = $input->read();
+        }
+        return $values;
     }
 }
